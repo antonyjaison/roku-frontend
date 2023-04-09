58af93a6822977bbd223ae682b986cf28d5d023c
Antony Jaison <antonyjaison639@gmail.com>
2023-04-09 13:05:05 +0530

    added css to the kids component

--- public/js/components/TheAllUsersComponent.js
+++ public/js/components/TheAllUsersComponent.js
@@ -1,10 +1,10 @@
 // imports always go here
-import UserComponent from './TheUserComponent.js';
+import UserComponent from "./TheUserComponent.js";
 
 export default {
-    name: 'TheAllUsersComponent',
+  name: "TheAllUsersComponent",
 
-    template: `
+  template: `
     <section class="user-panel">
         <h2>Who's Using Roku?</h2>
 
@@ -13,26 +13,26 @@ export default {
     </section>
     `,
 
-    created() {
-        // this is where you should do all of your user data retrieval
-        console.log('all users is ready');
-        fetch('/ums/users')
-            .then(res => res.json())
-            .then(data => {
-                console.table(data);
-                // push the users into the vue data object so we can render a component for each user
-                this.users = data;
-            })
-        .catch(error => console.error(error));
-    },
+  created() {
+    // this is where you should do all of your user data retrieval
+    console.log("all users is ready");
+    fetch("http://localhost:5050/ums/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.table(data);
+        // push the users into the vue data object so we can render a component for each user
+        this.users = data;
+      })
+      .catch((error) => console.error(error));
+  },
 
-    data() {
-        return {
-            users: []
-        }
-    },
+  data() {
+    return {
+      users: [],
+    };
+  },
 
-    components: {
-        user: UserComponent
-    }
-}
+  components: {
+    user: UserComponent,
+  },
+};
--- public/js/components/TheKidsHomeComponent.js
+++ public/js/components/TheKidsHomeComponent.js
@@ -1,55 +1,48 @@
 export default {
-    name: "TheDefaultHomeComponent",
-    data() {
-      let moviesByGenre = {};
-      return {
-        moviesByGenre,
-      };
+  name: "TheDefaultHomeComponent",
+  data() {
+    let moviesByGenre = {};
+    return {
+      moviesByGenre,
+    };
+  },
+  created() {
+    this.fetchMovies();
+  },
+  methods: {
+    async fetchMovies() {
+      try {
+        const response = await fetch(
+          `http://localhost:5050/api/movie/animated`
+        );
+        const data = await response.json();
+        console.log(data);
+        this.moviesByGenre[0] = data.data.results;
+      } catch (error) {
+        console.error(error);
+      }
     },
-    created() {
-      this.fetchMovies();
-    },
-    methods: {
-      async fetchMovies() {
-        try {
-          const response = await fetch(`http://localhost:5050/api/movie/animated`);
-          const data = await response.json();
-          this.moviesByGenre[0] = data.data.results;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    },
-    template: `
-      <div>
-        <h1>The Kids Home Component</h1>
+  },
+  template: `
+      <div class="kids_wrapper">
         <div v-if="moviesByGenre[0]">
           <h2>Animated Movies</h2>
-          <ul>
-            <div style="display: flex; flex-wrap:wrap;">
+            <div class="movies">
               <div
                 v-for="movie in moviesByGenre[0]"
                 :key="movie.id"
-                style="
-                  flex: 0 0 auto;
-                  margin-right: 10px;
-                  width: 300px;
-                  border: 1px solid #ccc;
-                  padding: 10px;
-                "
               >
-                <h4>{{ movie.title }}</h4>
                 <img
                   :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
                   :alt="movie.title"
                   style="max-width: 100%; height: auto; margin-bottom: 10px;"
                 />
-                <p>{{ movie.overview }}</p>
-                <p><strong>Popularity:</strong> {{ movie.popularity }}</p>
+                <h4>{{ movie.title }}</h4>
+                <p><strong>Rating:</strong> {{ movie.vote_average }}</p>
+                <button>Play</button>
               </div>
             </div>
-          </ul>
         </div>
       </div>
     `,
-  };  
+};
